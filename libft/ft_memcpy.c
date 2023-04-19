e17c7a52db79ec93d6186b11759fadd88af49e8a
Otxobot <otxoboy64@gmail.com>
2023-04-19 10:22:49 +0200

    mac version

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -1,19 +1,38 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	char	*pdest;
-	char	*psrc;
+// void	*ft_memcpy(void *dest, const void *src, size_t n)
+// {
+// 	char	*pdest;
+// 	char	*psrc;
 
-	if (!dest && !src)
-		return (NULL);
-	pdest = (char *)dest;
-	psrc = (char *)src;
-	while (n)
+// 	if (!dest && !src)
+// 		return (NULL);
+// 	pdest = (char *)dest;
+// 	psrc = (char *)src;
+// 	while (n)
+// 	{
+// 		pdest[n - 1] = psrc[n - 1];
+// 		n--;
+// 	}
+// 	return (pdest);
+// }
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char		*d;
+	unsigned const char	*s;
+	size_t				i;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	i = 0;
+	if (!dst && !src)
+		return (0);
+	while (i < n)
 	{
-		pdest[n - 1] = psrc[n - 1];
-		n--;
+		d[i] = s[i];
+		i++;
 	}
-	return (pdest);
+	return (dst);
 }
